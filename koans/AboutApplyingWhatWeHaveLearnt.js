ef800f050d9f7f4c276d69fb4b023d0b552f5108
elchangtw <elchang.tw@gmail.com>
2019-10-13 14:27:34 +0800

    Update AboutApplyingWhatWeHaveLearnt.js

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -38,7 +38,11 @@ describe("About Applying What We Have Learnt", function() {
 
       /* solve using filter() & all() / any() */
 
-      expect(productsICanEat.length).toBe(0);
+      productsICanEat = products.filter(function(product)){
+
+      }
+
+      expect(productsICanEat.length).toBe(1);
   });
 
   /*********************************************************************************/
@@ -57,6 +61,8 @@ describe("About Applying What We Have Learnt", function() {
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
     var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
+
+
 
     expect(233168).toBe(sum);
   });
